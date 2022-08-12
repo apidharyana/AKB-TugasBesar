20c0048d5d5323c40e2be9a08e73b896b8e6a56a
apidharyana <work.apidharyana@gmail.com>
2022-08-13 00:57:57 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pahlawannasional/main/ModelMain.kt
@@ -0,0 +1,16 @@
+package com.example.pahlawannasional.main
+
+import java.io.Serializable
+
+class ModelMain : Serializable {
+    lateinit var nama: String
+    lateinit var namaLengkap: String
+    lateinit var kategori: String
+    lateinit var asal: String
+    lateinit var lahir: String
+    lateinit var usia: String
+    lateinit var gugur: String
+    lateinit var lokasimakam: String
+    lateinit var history: String
+    lateinit var image: String
+}
